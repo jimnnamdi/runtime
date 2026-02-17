c5d9506f1b47d14f1983e63de5d46c32e3273a69
jimnnamdi <nnamdisamjim@gmail.com>
2026-02-17 08:19:02 -0800

    task structure and implementations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::VecDeque, pin::Pin, sync::{Arc, Mutex}, task::{Context, Poll, RawWaker, RawWakerVTable, Waker}};
+
+
+
+struct Executor {
+    queue: Mutex<VecDeque<Arc<Task>>>
 }
+
+struct Task {
+    future: Mutex<Pin<Box<dyn Future<Output = ()>>>>,
+    executor: Executor
+}
+static VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+
+impl Task {
+    fn raw_waker(task: Arc<Task>) -> RawWaker {
+        RawWaker::new(Arc::into_raw(task) as *const (), &VTABLE)
+    }
+
+    fn create_waker(self: &Arc<Self>) -> Waker {
+        unsafe { Waker::from_raw(Self::raw_waker(self.clone())) }
+    }
+
+    fn poll(self: &Arc<Task>) {
+        let waker = self.clone().create_waker();
+        let mut context = Context::from_waker(&waker);
+        let mut future = unsafe { Pin::new_unchecked(self.future.lock().unwrap())};
+
+        if let Poll::Pending = future.as_mut().poll(&mut context) {}
+    }
+
+    fn schedule(self: &Arc<Self>) {
+        self.executor.queue.lock().unwrap().push_back(self.clone());
+    }
+}
+
+fn main() {}
